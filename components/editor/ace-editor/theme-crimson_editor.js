b8c142912dcbbbaa80ccdaa9965151cb98ba6d9d
Fluidbyte <kent@fluidbyte.net>
2012-10-08 16:00:03 -0700

    Fix for theme backgrounds (dark on light themes)

--- components/editor/ace-editor/theme-crimson_editor.js
+++ components/editor/ace-editor/theme-crimson_editor.js
@@ -39,6 +39,7 @@ define('ace/theme/crimson_editor', ['require', 'exports', 'module', 'ace/lib/dom
 exports.isDark = false;
 exports.cssText = ".ace-crimson-editor .ace_editor {\
   border: 2px solid rgb(159, 159, 159);\
+  backgound: #fff;\
 }\
 \
 .ace-crimson-editor .ace_editor.ace_focus {\
