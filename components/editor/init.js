429db359a1ea241b5d8a1de9bc77b933ecfab604
fluidbyte <kent@fluidbyte.net>
2012-11-05 22:15:31 +0000

    Added C,C++,Ruby and Python support

--- components/editor/init.js
+++ components/editor/init.js
@@ -603,6 +603,12 @@
                 return 'sql';
             case 'md':
                 return 'markdown';
+            case 'cpp':
+                return 'c_cpp';
+            case 'py':
+                return 'python';
+            case 'rb':
+                return 'ruby';
             default:
                 return 'text';
             }
