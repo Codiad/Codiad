5963589821592c542944fea5bbc9b68fc5f516cc
Lorefnon <lorefnon@gmail.com>
2012-11-28 02:42:10 +0530

    Incorporate Google diff-match-patch library and add code for patch-generation on client

--- components/worker_manager/worker.js
+++ components/worker_manager/worker.js
@@ -1,8 +1,12 @@
+importScripts('diff_match_patch.js');
+
 tasks = {
     diff: function(config){
+        var dmf = new diff_match_patch();
+        var patchTxt = dmf.patch_toText(dmf.patch_make(config.original, config.changed));
         return {
             success: true,
-            result: "<<Some dummy diff>>"
+            result: patchTxt
         };
     }
 }
