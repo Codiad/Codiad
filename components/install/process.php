b7ad5fbb850c2705fcc06e2fe31130da1f871eda
daeks <daeks@gmx.net>
2013-04-01 12:19:56 +0200

    themes

--- components/install/process.php
+++ components/install/process.php
@@ -94,9 +94,16 @@ if(!file_exists($users) && !file_exists($projects) && !file_exists($active)){
 $rel = "' . $path . '";
 define("BASE_PATH",$rel);
 define("COMPONENTS",BASE_PATH . "/components");
+define("THEMES",BASE_PATH . "/themes");
 define("DATA",BASE_PATH . "/data");
 define("WORKSPACE",BASE_PATH . "/workspace");
 define("WSURL",$_SERVER["HTTP_HOST"] . $rel . "/workspace");
+
+//////////////////////////////////////////////////////////////////
+// THEME
+//////////////////////////////////////////////////////////////////
+
+define("THEME", "default");
 
 //////////////////////////////////////////////////////////////////
 // TIMEZONE
