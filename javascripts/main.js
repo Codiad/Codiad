50a0a46989f105bd9111355dfcd5b3480c1753af
Christian von Arnim <christian.vonarnim@gmail.com>
2013-04-04 22:58:58 +0200

    Revert "Merge branch 'gh-pages' of git@github.com:ccvca/Codiad.git"
    This reverts commit bce7b3e1d087471481572b73abed9b10d0f75cda, reversing
    changes made to c45c1403c2c1101f2948d185697d09e6531dd65f.

--- javascripts/main.js
+++
@@ -1,8 +0,0 @@
-function openModal(){
-  $('#modal').css({'top':(window.scrollY+50)+'px'});
-  $('#modal,#modal-overlay').fadeIn(300);
-};
-
-function closeModal(){
-  $('#modal,#modal-overlay').fadeOut(300);
-}
