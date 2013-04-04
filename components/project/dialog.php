00bec10aa3d2c8c6046546c9eb31b3ae212f65ac
fluidbyte <kent@fluidbyte.net>
2013-04-04 06:21:47 -0700

    Picky little UI changes on sidebar project-list

--- components/project/dialog.php
+++ components/project/dialog.php
@@ -42,7 +42,7 @@
                 if($projects_assigned && !in_array($data['path'],$projects_assigned)){ $show=false; }
                 if($show){
                 ?>
-                <li onclick="codiad.project.open('<?php echo($data['path']); ?>');"><div class="icon-folder icon"></div><?php echo($data['name']); ?></li>
+                <li onclick="codiad.project.open('<?php echo($data['path']); ?>');"><div class="icon-archive icon"></div><?php echo($data['name']); ?></li>
                 
                 <?php
                 }
