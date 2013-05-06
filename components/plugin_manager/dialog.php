e7679687a54efd49503f84ee2f810359d21a83b6
daeks <daeks@gmx.net>
2013-05-06 19:13:58 +0200

    check for required dependencies

--- components/plugin_manager/dialog.php
+++ components/plugin_manager/dialog.php
@@ -74,7 +74,7 @@
                                     <?php
                                 } else {
                                     if(checkAccess()){
-                                        if(is_writeable(PLUGINS) && extension_loaded('zip')) {
+                                        if(is_writeable(PLUGINS) && extension_loaded('zip') && extension_loaded('openssl') && ini_get('allow_url_fopen') == 1) {
                                         ?>
                                          <td><table style="text-align:center;border-spacing:0;border-collapse:collapse;"><tr><td style="border: 0;padding: 0;"><a class="icon-download icon" onclick="codiad.plugin_manager.install('<?php echo $plugin['name']; ?>','<?php echo $plugin['url']; ?>');return false;"></a></td><td style="border: 0;padding: 0;"><a class="icon-github icon" onclick="codiad.plugin_manager.openInBrowser('<?php echo $data[0]['url']; ?>');return false;"></a></td></tr></table></td>   
                                         <?php       
@@ -243,7 +243,7 @@
                                     <td><?php if($remote[0]['version'] != '') { echo($remote[0]['version']); } else { echo 'n/a'; } ?></td>
                                     <?php
                                     if($remote[0]['version'] != $data[0]['version']) {
-                                        if(is_writeable(PLUGINS) && extension_loaded('zip')) {
+                                        if(is_writeable(PLUGINS) && extension_loaded('zip') && extension_loaded('openssl') && ini_get('allow_url_fopen') == 1) {
                                         ?>
                                             <td><table style="text-align:center;border-spacing:0;border-collapse:collapse;"><tr><td style="border: 0;padding: 0;"><a class="icon-download icon" onclick="codiad.plugin_manager.update('<?php echo $fname; ?>');return false;"></a></td><td style="border: 0;padding: 0;"><a class="icon-github icon" onclick="codiad.plugin_manager.openInBrowser('<?php echo $data[0]['url']; ?>');return false;"></a></td></tr></table></td>
                                         <?php
