80a3e0b54d22635bb418e5ad63dcb52e4bc366f6
fluidbyte <kent@fluidbyte.net>
2013-05-18 17:39:39 +0000

    Added top drop-in banner

--- javascripts/main.js
+++ javascripts/main.js
@@ -12,5 +12,12 @@ $(function(){
    $.getJSON('https://api.github.com/repos/Codiad/Codiad/tags', function(data){
       $('.version').text(data[0].name); 
    });
+   
+   setTimeout(function(){
+       if(!localStorage.getItem('banner')) {
+           $('#demo_drop-in').animate({'margin-top':'-70px'},1000);
+           localStorage.setItem('banner', 'true');
+       }
+   },2000);
     
 });
