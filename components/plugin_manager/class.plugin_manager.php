aca3a3e3d64d8d4ae07cbeafd16de6b9eebbd202
daeks <daeks@gmx.net>
2013-05-20 13:57:02 +0200

    legacy update

--- components/plugin_manager/class.plugin_manager.php
+++ components/plugin_manager/class.plugin_manager.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
-*  Copyright (c) Codiad & Kent Safranski (codiad.com), distributed
+*  Copyright (c) Codiad & daeks (codiad.com), distributed
 *  as-is and without warranty under the MIT License. See
 *  [root]/license.txt for more. This information must remain intact.
 */
