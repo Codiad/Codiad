c8a35b82160ebd23536781b24ff6d4b704805c2c
MrChang1 <f1687506@rmqkr.net>
2013-06-09 14:45:43 +0200

    Update dialog.php
    I modified the php for translation purposes only.

--- components/update/dialog.php
+++ components/update/dialog.php
@@ -24,9 +24,9 @@
         
             if(!checkAccess()){ 
             ?>
-            <label>Restricted</label>
-            <pre>You can not check for updates</pre>
-            <button onclick="codiad.modal.unload();return false;">Close</button>
+            <label><?php i18n("Restricted"); ?></label>
+            <pre><?php i18n("You can not check for updates"); ?></pre>
+            <button onclick="codiad.modal.unload();return false;"><?php i18n("Close"); ?></button>
             <?php } else {
                 require_once('class.update.php');
                 $update = new Update();
@@ -35,24 +35,24 @@
             <form>
             <input type="hidden" name="archive" value="<?php echo $vars[0]['data']['archive']; ?>">
             <input type="hidden" name="remoteversion" value="<?php echo $vars[0]['data']['remoteversion']; ?>">
-            <label>Update Check</label>
+            <label><?php i18n("Update Check"); ?></label>
             <br><table>
-                <tr><td>Your Version</td><td><?php echo $vars[0]['data']['currentversion']; ?></td></tr>
-                <tr><td>Latest Version</td><td><?php echo $vars[0]['data']['remoteversion']; ?></td></tr>
+                <tr><td><?php i18n("Your Version"); ?></td><td><?php echo $vars[0]['data']['currentversion']; ?></td></tr>
+                <tr><td><?php i18n("Latest Version"); ?></td><td><?php echo $vars[0]['data']['remoteversion']; ?></td></tr>
             </table>
             <?php if($vars[0]['data']['currentversion'] != $vars[0]['data']['remoteversion']) { ?>
-            <br><label>Changes on Codiad</label>
+            <br><label><?php i18n("Changes on Codiad"); ?></label>
             <pre style="overflow: auto; max-height: 200px; max-width: 450px;"><?php echo $vars[0]['data']['message']; ?></pre>
             <?php } else { ?>
-            <br><br><b><label>Congratulation, your system is up to date.</label></b>
+            <br><br><b><label><?php i18n("Congratulation, your system is up to date."); ?></label></b>
             <?php if($vars[0]['data']['name'] != '') { ?>
-            <em>Last update was done by <?php echo $vars[0]['data']['name']; ?>.</em>
+            <em><?php i18n("Last update was done by "); ?><?php echo $vars[0]['data']['name']; ?>.</em>
             <?php } } ?>
             <br><?php
                 if($vars[0]['data']['currentversion'] != $vars[0]['data']['remoteversion']) {
                     echo '<button class="btn-left" onclick="codiad.update.download();return false;">Download Codiad</button>&nbsp;';
                 }
-            ?><button class="btn-right" onclick="codiad.modal.unload();return false;">Cancel</button>
+            ?><button class="btn-right" onclick="codiad.modal.unload();return false;"><?php i18n("Cancel"); ?></button>
             <form>
             <?php }
             break;
