0fb7502560212eab97db84d0a129fac1e3dfa3b3
Evertton de Lima <e.everttonlima@gmail.com>
2013-06-09 16:42:17 -0300

    Reverted components/project/class.project.php to revision https://github.com/Codiad/Codiad/commit/f531816a21ee07bd59f3028955a2a77274e4b393.

--- components/project/class.project.php
+++ components/project/class.project.php
@@ -39,7 +39,7 @@ class Project extends Common {
             $this->assigned = getJSON($_SESSION['user'] . '_acl.php');
         }
     }
-
+    
     //////////////////////////////////////////////////////////////////
     // Get First (Default, none selected)
     //////////////////////////////////////////////////////////////////
@@ -110,7 +110,7 @@ class Project extends Common {
             if(strtoupper(substr(PHP_OS, 0, 3)) === 'WIN' || !$this->isAbsPath($this->path)) {
                 $this->path = $this->SanitizePath();
             }
-            if($this->path != '' && $this->path != '..') {
+            if($this->path != '') {
                 $pass = $this->checkDuplicate();
                 if($pass){
                     if(!$this->isAbsPath($this->path)) {
@@ -139,7 +139,7 @@ class Project extends Common {
                     }
                     $this->projects[] = array("name"=>$this->name,"path"=>$this->path);
                     saveJSON('projects.php',$this->projects);
-
+                    
                     // Pull from Git Repo?
                     if($this->gitrepo){
                         if(!$this->isAbsPath($this->path)) {
@@ -149,7 +149,7 @@ class Project extends Common {
                         }
                         $this->ExecuteCMD();
                     }
-
+                    
                     echo formatJSEND("success",array("name"=>$this->name,"path"=>$this->path));
                 }else{
                     echo formatJSEND("error","A Project With the Same Name or Path Exists");
@@ -161,7 +161,7 @@ class Project extends Common {
              echo formatJSEND("error","Project Name/Folder is empty");
         }
     }
-
+    
     //////////////////////////////////////////////////////////////////
     // Rename
     //////////////////////////////////////////////////////////////////
@@ -218,13 +218,13 @@ class Project extends Common {
 
     public function SanitizePath(){
         $sanitized = str_replace(" ","_",$this->path);
-        return preg_replace('/[^\w-\.]/', '', $sanitized);
+        return preg_replace('/[^\w-]/', '', $sanitized);
     }
-
+    
     //////////////////////////////////////////////////////////////////
     // Clean Path
     //////////////////////////////////////////////////////////////////
-
+    
     function cleanPath(){
 
         // prevent Poison Null Byte injections
@@ -233,16 +233,14 @@ class Project extends Common {
         // prevent go out of the workspace
         while (strpos($path , '../') !== false)
             $path = str_replace( '../', '', $path );
-        while(strpos($path, '/..') !== false)
-            $path = str_replace('/..', '', $path);
 
         return $path;
     }
-
+    
     //////////////////////////////////////////////////////////////////
     // Execute Command
     //////////////////////////////////////////////////////////////////
-
+    
     public function ExecuteCMD(){
         if(function_exists('system')){
             ob_start();
