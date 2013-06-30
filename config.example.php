94e113577f92bacd7f0a88518f629ec8ff4f4d52
daeks <daeks@gmx.net>
2013-06-30 19:53:14 +0200

    beta testing

--- config.example.php
+++ config.example.php
@@ -47,7 +47,7 @@ define("WSURL", BASE_URL . "/workspace");
 define("PMURL", "http://codiad.com/plugins.json");
 
 // Update Check
-define("UPDATEURL", "http://update.codiad.com/?v={VER}&o={OS}&p={PHP}&i={IP}");
-define("ARCHIVEURL", "https://github.com/Codiad/Codiad/archive/master.zip");
-define("COMMITURL", "https://api.github.com/repos/Codiad/Codiad/commits");
+//define("UPDATEURL", "http://update.codiad.com/?v={VER}&o={OS}&p={PHP}&i={IP}");
+//define("ARCHIVEURL", "https://github.com/Codiad/Codiad/archive/master.zip");
+//define("COMMITURL", "https://api.github.com/repos/Codiad/Codiad/commits");
 
