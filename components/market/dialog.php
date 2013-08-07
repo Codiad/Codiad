3da2090e4bbfaeda0e66a69e83092a5705919404
daeks <daeks@gmx.net>
2013-08-07 17:36:21 +0200

    first commit

--- components/market/dialog.php
+++ components/market/dialog.php
@@ -18,93 +18,88 @@
     switch($_GET['action']){
     
         //////////////////////////////////////////////////////////////
-        // Plugin Market
+        // Marketplace
         //////////////////////////////////////////////////////////////
         
-        case 'market':
+        case 'list':
         
-            require_once('class.plugin_manager.php');
-            $pm = new Plugin_manager();
-            $market = $pm->Market();
+            require_once('class.market.php');
+            $market = new Market();
             ?>
-            <label><?php i18n("Plugin Market"); ?></label>
-            <div id="plugin-list">
+            <label><?php i18n("Codiad Marketplace"); ?></label>
+            <div id="market-list">
             <table width="100%">
                 <tr>
-                    <th><?php i18n("Plugin Name"); ?></th>
-                    <th><?php i18n("Description"); ?></th>
-                    <th><?php i18n("Author"); ?></th>
-                    <th><?php i18n("Download"); ?></th>
+                    <th valign="middle"><button style="margin:0;" class="btn-left" onclick="codiad.market.list();return false;">All</button><button class="btn-mid" style="margin:0;"  onclick="codiad.market.list('plugins');return false;">Plugins</button><button class="btn-right" style="margin:0;" onclick="codiad.market.list('themes');return false;">Themes</button></th>
+                    <th valign="middle" width="30%"><input style="margin:0;display:inline" placeholder="Search"></th>
                 </tr>
+             </table>
+             <div style="height: 450px; width: 100%; overflow-y: auto; overflow-x: hidden;">
+             <table width="100%">
             <?php
-            if($market != '') {
-                $marketlist = array();
-                foreach($market as $plugin) {
-                    if(!isset($plugin['category']) || $plugin['category'] == '') {
-                        $plugin['category'] = 'Common';
+                $marketplace = array();
+                foreach($market->remote as $data) {
+                    if(!isset($data['category']) || $data['category'] == '') {
+                        $data['category'] = 'Common';
                     }
-                    if(!array_key_exists($plugin['category'], $marketlist)) {
-                        $marketlist[$plugin['category']] = array();
+                    if(!array_key_exists($data['type'], $marketplace)) {
+                        $marketplace[$data['type']] = array();
+                    }
+                    if(!array_key_exists($data['category'], $marketplace[$data['type']])) {
+                        $marketplace[$data['type']][$data['category']] = array();
                     } 
-                    array_push($marketlist[$plugin['category']], $plugin);
+                    array_push($marketplace[$data['type']][$data['category']], $data);
                 }
+                ksort($marketplace);
                 
-                ksort($marketlist);
-                $isDownloadable = (is_writeable(PLUGINS) && extension_loaded('zip') && extension_loaded('openssl') && ini_get('allow_url_fopen') == 1);
+                function sort_name($a, $b) { return strnatcmp($a['name'], $b['name']); }
                 
-                foreach($marketlist as $category=>$pluginlist) {
-                    ?>
-                    <tr>
-                    <th colspan="4"><?php echo $category;?></th>
-                    </tr>
-                    <?php
-                    foreach($pluginlist as $plugin) {
-                        if(substr($plugin['url'],-4) == '.git') {
-                            $plugin['url'] = substr($plugin['url'],0,-4);
-                        }
-                    
-                        ?>
-                        <tr>
-                            <td><?php echo $plugin['name']; ?></td>
-                            <td width="400px"><?php echo $plugin['description']; ?></td>
-                            <td><?php echo $plugin['author']; ?></td>
-                            <?php
-                                if(file_exists(PLUGINS.substr($plugin['url'],strrpos($plugin['url'],'/'))) || file_exists(PLUGINS.substr($plugin['url'],strrpos($plugin['url'],'/')).'-master')) {
-                                    ?>
-                                    <td><div class="icon-check icon"></div></td>
-                                    <?php
-                                } else {
-                                    if(checkAccess()){
-                                        if($isDownloadable) {
-                                        ?>
-                                         <td><table style="text-align:center;border-spacing:0;border-collapse:collapse;"><tr><td style="border: 0;padding: 0;"><a class="icon-download icon" onclick="codiad.plugin_manager.install('<?php echo $plugin['name']; ?>','<?php echo $plugin['url']; ?>');return false;"></a></td><td style="border: 0;padding: 0;"><a class="icon-github icon" onclick="codiad.plugin_manager.openInBrowser('<?php echo $plugin['url']; ?>');return false;"></a></td></tr></table></td>   
-                                        <?php       
-                                        } else {
-                                        ?>
-                                        <td><a class="icon-download icon" onclick="codiad.plugin_manager.openInBrowser('<?php echo $plugin['url']; ?>');return false;"></a></td>
-                                        <?php
-                                        }                             
-                                    } else {
-                                        ?>
-                                        <td><div class="icon-block icon"></div></td>
-                                        <?php
-                                    }
-                                }
-                            ?>
-                        </tr>
-                        <?php
+                foreach($marketplace as $type=>$data) {
+                    ksort($data);
+                    if($_GET['type'] == 'undefined' || $_GET['type'] == $type) {
+                      foreach($data as $category=>$subdata) {
+                          usort($subdata, 'sort_name');
+                          foreach($subdata as $addon){
+                            echo '<tr><td><div style="position:relative;height:110px">';
+                            $left = 0;
+                            if($addon['image'] != '') {
+                              echo '<div style="margin-top:5px;"><a onclick="codiad.market.openInBrowser(\''.$addon['image'].'\');return false;"><img src="'.$addon['image'].'" width="150" height="100"></a></div>';
+                              $left = 150;
+                            } else {
+                              if(isset($addon['new'])) {
+                                $left = $left + 40;
+                              }
+                            }
+                            if(isset($addon['new'])) {
+                                echo '<div style="position:absolute;top:0px;left:0px;z-index:10000;"><img src="./themes/default/images/new.png"></div>';
+                            }
+                            echo '<div style="position:absolute;top:0px;left:'.($left+10).'px;"><a style="font-weight:bold;" onclick="codiad.market.openInBrowser(\''.$addon['url'].'\');return false;">'.$addon['name'].'</a></div>';
+                            echo '<div style="position:absolute;top:15px;left:'.($left+10).'px;"><font style="font-size:10px">'.ucfirst(rtrim($type,'s')).' - '.ucfirst($category).' | <a style="font-weight:bold;text-decoration:underline;" onclick="codiad.market.openInBrowser(\'https://github.com/'.$addon['author'].'\');return false;">'.$addon['author'].'</a> | '.$addon['count'].' Users</font></div>';
+                            echo '<div style="position:absolute;top:25px;left:'.($left+5).'px;"><pre style="height:60px;color:#a8a6a8;width:'.(550-$left).'px;white-space: pre-wrap;">'.$addon['description'].'</pre></div>';
+                            if(isset($addon['version'])) {
+                              if(!isset($addon['update'])) {
+                                  echo '<div style="position:absolute;top:5px;left:575px;"><font style="color:green">Latest Version v'.$addon['version'].'</font></div>';
+                              } else {
+                                 echo '<div style="position:absolute;top:-5px;left:575px;"><button style="color: blue; width:140px" onclick="codiad.market.update(\''.$type.'\', \''.$addon['folder'].'\');return false;">Update '.ucfirst(rtrim($type,'s')).'</button></div>';
+                              }
+                              echo '<div style="position:absolute;top:30px;left:575px;"><button style="color: red; width:140px" onclick="codiad.market.remove(\''.$type.'\', \''.$addon['folder'].'\');return false;">Delete '.ucfirst(rtrim($type,'s')).'</button><div>';
+                            } else {
+                              echo '<div style="position:absolute;top:-5px;left:575px;"><button style="width:140px" onclick="codiad.market.install(\''.$type.'\', \''.$addon['name'].'\',\''.$addon['url'].'\');return false;">Install '.ucfirst(rtrim($type,'s')).'</button><div>';
+                            }
+                            echo '</div></td></tr>';
+                          }
+                      }
                     }
-                }
-            } else {
-                ?>
-                <tr><td colspan="4"><?php i18n("Plugin Market currently unavailable."); ?></td></tr>
-                <?php
-            }
-            
+                }            
             ?>
-            </table>
+            </table></div>
             </div>
-            <button class="btn-left" onclick="codiad.plugin_manager.list();return false;"><?php i18n("Installed Plugins"); ?></button><button class="btn-mid" onclick="codiad.plugin_manager.check();return false;"><?php i18n("Update Check"); ?></button><button class="btn-right" onclick="codiad.modal.unload();return false;"><?php i18n("Close"); ?></button>
+            <table width="100%">
+                <tr>
+                    <th valign="middle"><input style="margin:0;display:inline" placeholder="Enter GitHub Repository Url..."></th>
+                    <th valign="middle" align="right" width="222px"><button class="btn-left" onclick="codiad.market.list();return false;"><?php i18n("Install Manually"); ?></button><button class="btn-right" onclick="codiad.modal.unload();return false;"><?php i18n("Close"); ?></button></th>
+                </tr>
+             </table>
             <?php
             
             break;
@@ -113,7 +108,7 @@
         // List Plugins
         //////////////////////////////////////////////////////////////
         
-        case 'list':
+        case 'list2':
             ?>
             <label><?php i18n("Plugin List"); ?></label>
             <div id="plugin-list">
--- plugins/README.md
+++
@@ -1,5 +0,0 @@
-# Plugins
-
-The plugins directory is where you place plugins :-)
-
-[GET SOME...](http://www.codiad.com/plugins.html)
