ce24538ebd04eeb7fff162973495695ce71069d0
daeks <daeks@users.noreply.github.com>
2014-07-27 00:36:12 +0200

    Update process.php

--- components/install/process.php
+++ components/install/process.php
@@ -111,7 +111,7 @@ if(!file_exists($users) && !file_exists($projects) && !file_exists($active)){
     // Create Users file
     //////////////////////////////////////////////////////////////////
 
-    $user_data = array("username"=>$username,"password"=>$password,"project"=>$project_path,"challenge"=>"");
+    $user_data = array("username"=>$username,"password"=>$password,"project"=>$project_path);
 
     saveJSON($users,array($user_data));
 
