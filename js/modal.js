934b5d96f1f0891a9331dfe29a945bc4878dd8fe
Andr3as <andr3as@me>
2014-08-10 20:08:01 +0200

    Persistent modal settings

--- js/modal.js
+++ js/modal.js
@@ -10,10 +10,11 @@
 
         load: function(width, url, data) {
             data = data || {};
+            var bounds = this._getBounds(width);
             $('#modal')
                 .css({
-                    'top': '15%',
-                    'left': '50%',
+                    'top': bounds.top,
+                    'left': bounds.left,
                     'min-width': width + 'px',
                     'margin-left': '-' + Math.ceil(width / 2) + 'px'
                 })
@@ -39,6 +40,7 @@
         },
 
         unload: function() {
+            this._setBounds();
             $('#modal-content form')
                 .die('submit'); // Prevent form bubbling
             $('#modal, #modal-overlay')
@@ -55,6 +57,44 @@
                     .animate({
                         'margin-left': '10px'
                 }, 300, 'easeOutQuart');
+            }
+        },
+
+        _setBounds: function(bounds) {
+            if (typeof(bounds) == 'undefined') {
+                if ($('#modal').is(':visible')) {
+                    bounds      = {};
+                    bounds.top  = Math.floor($('#modal').offset().top);
+                    bounds.left = Math.floor($('#modal').offset().left);
+                } else {
+                    return false;
+                }
+            }
+            //Save bounds
+            localStorage.setItem("codiad.modal.top", bounds.top);
+            localStorage.setItem("codiad.modal.left", bounds.left);
+        },
+
+        _getBounds: function(width) {
+            if (localStorage.getItem("codiad.modal.top") !== null && localStorage.getItem("codiad.modal.left") !== null) {
+                var top     = localStorage.getItem('codiad.modal.top'),
+                    left    = localStorage.getItem('codiad.modal.left');
+                //Check if modal is out of window
+                if ((parseInt(top, 10) + 40) > $(window).height()) {
+                    top = "15%";
+                }
+                if ((parseInt(left, 10) + parseInt(width, 10) + 40) > $(window).width()) {
+                    left = "50%";
+                }
+                return {
+                    top: top,
+                    left: left
+                };
+            } else {
+                return {
+                    top: "15%",
+                    left: "50%"
+                };
             }
         }
 
