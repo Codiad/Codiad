fbd206a4c7f87e490dc01b85c2c9101be32dcf34
marin1993 <sang93@qq.com>
2019-01-11 09:52:40 +0800

    文件名允许使用中文

--- components/filemanager/class.filemanager.php
+++ components/filemanager/class.filemanager.php
@@ -609,7 +609,7 @@ class Filemanager extends Common
         $path = str_replace('\\', '/', $path);
 
         // allow only valid chars in paths$
-        $path = preg_replace('/[^A-Za-z0-9\-\._\/]/', '', $path);
+        $path = preg_replace('/[^_A-Za-z0-9\-\._\/\x{4e00}-\x{9fff}]/u', '', $path);
         // maybe this is not needed anymore
         // prevent Poison Null Byte injections
         $path = str_replace(chr(0), '', $path);
