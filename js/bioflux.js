f36f6dc0470880bd3aa6fb55134199458ceedb09
liam <liam@siira.us>
2019-09-29 11:23:52 -0400

    Added temporary get script function, unused

--- js/bioflux.js
+++ js/bioflux.js
@@ -177,3 +177,23 @@
 	return publicAPIs;
 
 });
+
+
+
+function getScript(source, callback) {
+    var script = document.createElement('script');
+    var prior = document.getElementsByTagName('script')[0];
+    script.async = 1;
+
+    script.onload = script.onreadystatechange = function( _, isAbort ) {
+        if(isAbort || !script.readyState || /loaded|complete/.test(script.readyState) ) {
+            script.onload = script.onreadystatechange = null;
+            script = undefined;
+
+            if(!isAbort && callback) setTimeout(callback, 0);
+        }
+    };
+
+    script.src = source;
+    prior.parentNode.insertBefore(script, prior);
+}
